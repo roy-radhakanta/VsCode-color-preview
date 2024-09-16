a9ab5c8a99a2f7b8269718e8f4f8990555998d64
Christoph Tupi <ctupi.mmt-b2021@fh-salzburg.ac.at>
2024-09-16 09:42:59 +0200

    preview colors on extension activated + refactored previewColors

--- extension.js
+++ extension.js
@@ -20,15 +20,19 @@ function activate(context) {
     context.globalState.update('firstActivation', false);
   }
 
+  const supportedLanguages = ["scss", "sass", "css", "javascript", "typescript", "typescriptreact", "javascriptreact", "html", "json"];
+  if (vscode.window.activeTextEditor && supportedLanguages.includes(vscode.window.activeTextEditor.document.languageId)) {
+    previewColors(vscode.window.activeTextEditor.document);
+  }
+
   const disposable = vscode.commands.registerCommand(
     "color-preview.colorPreview",
     function () {
-      const languageSupport = ["scss", "sass", "css", "javascript", "typescript", "typescriptreact", "javascriptreact", "html", "json"];
 
       // on open
       context.subscriptions.push(
         vscode.workspace.onDidOpenTextDocument(function (document) {
-          if (languageSupport.includes(document.languageId)) {
+          if (supportedLanguages.includes(document.languageId)) {
             previewColors(document);
           }
         })
@@ -37,8 +41,7 @@ function activate(context) {
       // on tab change
       context.subscriptions.push(
         vscode.window.onDidChangeActiveTextEditor((editor) => {
-          //   console.log(editor.document.languageId);
-          if (editor && languageSupport.includes(editor.document.languageId)) {
+          if (editor && supportedLanguages.includes(editor.document.languageId)) {
             previewColors(editor.document);
           }
         })
@@ -47,14 +50,14 @@ function activate(context) {
       // on save
       context.subscriptions.push(
         vscode.workspace.onDidSaveTextDocument((document) => {
-          if (languageSupport.includes(document.languageId)) {
+          if (supportedLanguages.includes(document.languageId)) {
             previewColors(document);
           }
         })
       );
 
       vscode.window.showInformationMessage(
-        "Color Preview is ow ready to work 🚀"
+        "Color Preview is now ready to work 🚀"
       );
     }
   );
@@ -120,27 +123,34 @@ function previewColors(openedFile) {
     });
   });
 
-  /** @type {vscode.TextEditor}*/
-  const activeEditor = vscode.window.activeTextEditor;
+  /**
+   * Apply decorations to the opened file
+   * @function applyDecorations
+   * @param {vscode.TextDocument} openedFile
+   * @param {vscode.DecorationOptions[]} colorDecorations
+   * @returns {void}
+   */
+  function applyDecorations(openedFile, colorDecorations) {
+    /** @type {vscode.TextEditor}*/
+    const activeEditor = vscode.window.activeTextEditor;
 
-  if (activeEditor && activeEditor.document === openedFile) {
-    if (colorDecorationType) {
-      activeEditor.setDecorations(colorDecorationType, []);
+    if (activeEditor && activeEditor.document === openedFile) {
+      if (colorDecorationType) {
+        activeEditor.setDecorations(colorDecorationType, []);
+      }
+
+      /** @type {vscode.TextEditorDecorationType} */
+      colorDecorationType = vscode.window.createTextEditorDecorationType({});
+      activeEditor.setDecorations(colorDecorationType, colorDecorations);
     }
-
-    /** @type {vscode.TextEditorDecorationType} */
-    colorDecorationType = vscode.window.createTextEditorDecorationType({});
-
-    activeEditor.setDecorations(colorDecorationType, colorDecorations);
   }
-}
 
 
-function deactivate() {
+  function deactivate() {
 
-}
+  }
 
-module.exports = {
-  activate,
-  deactivate,
-};
+  module.exports = {
+    activate,
+    deactivate,
+  };
